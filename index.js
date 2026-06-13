32795ae83d2ae01fc01e18ba60b861afd8039d19
Dev Patel <22devpatel007@gmail.com>
2026-06-13 16:58:23 -0330

    inital project

--- index.js
+++
@@ -1,8 +0,0 @@
-import { registerRootComponent } from 'expo';
-import TrackPlayer from 'react-native-track-player';
-import { PlaybackService } from './src/player/PlaybackService';
-import App from './App';
-
-TrackPlayer.registerPlaybackService(() => PlaybackService);
-
-registerRootComponent(App);
--- src/services/search.service.ts
+++
@@ -1,6 +0,0 @@
-import api from './api';
-
-export const searchSongs = async (query: string, limit = 20): Promise<any[]> => {
-  const res = await api.get('/search', { params: { q: query, limit } });
-  return res.data?.data ?? res.data ?? [];
-};
